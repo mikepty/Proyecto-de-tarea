e842be4ade7600250c28228e672f42b07ad73d29
Michael Guillén <mikezero0313@gmail.com>
2020-06-30 14:27:16 -0500

    Agregando archivos
    Adjunto archivos de app para calificación de primer proyecto del curso

---
+++ ProyectoMichael/app/src/main/java/com/aprender/proyecto_michael/MainActivity.java
@@ -0,0 +1,39 @@
+package com.aprender.proyecto_michael;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Bienvenidos", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "A mi", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Proyecto", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida.
+    }
+}
